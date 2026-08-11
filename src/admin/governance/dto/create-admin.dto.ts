f01eda19fb3b395b22bbab81f039ffb77d3dcb42
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 23:23:12 +0200

    Format administrator creation validation

--- src/admin/governance/dto/create-admin.dto.ts
+++ src/admin/governance/dto/create-admin.dto.ts
@@ -1,9 +1,4 @@
-import {
-  IsEmail,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 import {
   AUTH_PASSWORD_MAX_LENGTH,
@@ -15,23 +10,15 @@ import {
 export class CreateAdminDto {
   @IsString()
   @MinLength(2)
-  @MaxLength(
-    USER_FULL_NAME_MAX_LENGTH,
-  )
+  @MaxLength(USER_FULL_NAME_MAX_LENGTH)
   fullName!: string;
 
   @IsEmail()
-  @MaxLength(
-    USER_EMAIL_MAX_LENGTH,
-  )
+  @MaxLength(USER_EMAIL_MAX_LENGTH)
   email!: string;
 
   @IsString()
-  @MinLength(
-    AUTH_PASSWORD_MIN_LENGTH,
-  )
-  @MaxLength(
-    AUTH_PASSWORD_MAX_LENGTH,
-  )
+  @MinLength(AUTH_PASSWORD_MIN_LENGTH)
+  @MaxLength(AUTH_PASSWORD_MAX_LENGTH)
   password!: string;
 }
