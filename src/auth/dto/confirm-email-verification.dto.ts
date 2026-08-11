f10232150747243e0b13d99de35b7f290fb599d1
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 20:23:03 +0200

    Validate email verification confirmation tokens

---
+++ src/auth/dto/confirm-email-verification.dto.ts
@@ -0,0 +1,12 @@
+import {
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class ConfirmEmailVerificationDto {
+  @IsString()
+  @MinLength(32)
+  @MaxLength(512)
+  token!: string;
+}
