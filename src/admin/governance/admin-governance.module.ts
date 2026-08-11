d1703dcfd40531b3ad21c6338425f9d62bb1480d
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 23:23:12 +0200

    Format admin governance module registration

--- src/admin/governance/admin-governance.module.ts
+++ src/admin/governance/admin-governance.module.ts
@@ -1,42 +1,18 @@
-import {
-  Module,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import {
-  AuthModule,
-} from '../../auth/auth.module';
-import {
-  DatabaseModule,
-} from '../../database/database.module';
-import {
-  AdminAdminsController,
-} from './controllers/admin-admins.controller';
-import {
-  AdminCustomersController,
-} from './controllers/admin-customers.controller';
-import {
-  AccountGovernanceService,
-} from './services/account-governance.service';
-import {
-  AdminAdminsService,
-} from './services/admin-admins.service';
-import {
-  AdminCustomersService,
-} from './services/admin-customers.service';
-import {
-  AdminUserActivitiesService,
-} from './services/admin-user-activities.service';
+import { AuthModule } from '../../auth/auth.module';
+import { DatabaseModule } from '../../database/database.module';
+import { AdminAdminsController } from './controllers/admin-admins.controller';
+import { AdminCustomersController } from './controllers/admin-customers.controller';
+import { AccountGovernanceService } from './services/account-governance.service';
+import { AdminAdminsService } from './services/admin-admins.service';
+import { AdminCustomersService } from './services/admin-customers.service';
+import { AdminUserActivitiesService } from './services/admin-user-activities.service';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    AuthModule,
-  ],
+  imports: [DatabaseModule, AuthModule],
 
-  controllers: [
-    AdminCustomersController,
-    AdminAdminsController,
-  ],
+  controllers: [AdminCustomersController, AdminAdminsController],
 
   providers: [
     AccountGovernanceService,
