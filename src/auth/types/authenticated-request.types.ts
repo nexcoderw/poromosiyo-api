0d7d510cf05d728b314a121cf3c26c2db4848d8a
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 20:10:34 +0200

    Define authenticated HTTP request context

---
+++ src/auth/types/authenticated-request.types.ts
@@ -0,0 +1,12 @@
+import type {
+  Request,
+} from 'express';
+
+import type {
+  AuthPrincipal,
+} from './auth.types';
+
+export type AuthenticatedRequest =
+  Request & {
+    auth: AuthPrincipal;
+  };
