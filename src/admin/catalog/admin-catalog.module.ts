d75bdcf129bd6528ec44605be7bca70df393cd5a
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 21:24:37 +0200

    Establish Poromosiyo admin catalog module

---
+++ src/admin/catalog/admin-catalog.module.ts
@@ -0,0 +1,56 @@
+import {
+  Module,
+} from '@nestjs/common';
+
+import {
+  AuthModule,
+} from '../../auth/auth.module';
+import {
+  DatabaseModule,
+} from '../../database/database.module';
+import {
+  AdminBrandsController,
+} from './controllers/admin-brands.controller';
+import {
+  AdminCategoriesController,
+} from './controllers/admin-categories.controller';
+import {
+  AdminProductImagesController,
+} from './controllers/admin-product-images.controller';
+import {
+  AdminProductsController,
+} from './controllers/admin-products.controller';
+import {
+  AdminBrandsService,
+} from './services/admin-brands.service';
+import {
+  AdminCategoriesService,
+} from './services/admin-categories.service';
+import {
+  AdminProductImagesService,
+} from './services/admin-product-images.service';
+import {
+  AdminProductsService,
+} from './services/admin-products.service';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    AuthModule,
+  ],
+
+  controllers: [
+    AdminCategoriesController,
+    AdminBrandsController,
+    AdminProductsController,
+    AdminProductImagesController,
+  ],
+
+  providers: [
+    AdminCategoriesService,
+    AdminBrandsService,
+    AdminProductsService,
+    AdminProductImagesService,
+  ],
+})
+export class AdminCatalogModule {}
