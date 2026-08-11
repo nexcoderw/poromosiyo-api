16f395ebc179a749c51fb10866b3658da8c4225f
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 20:23:02 +0200

    Validate forgot password requests

---
+++ src/auth/dto/forgot-password.dto.ts
@@ -0,0 +1,14 @@
+import {
+  IsEmail,
+  MaxLength,
+} from 'class-validator';
+
+import {
+  USER_EMAIL_MAX_LENGTH,
+} from '../auth.constants';
+
+export class ForgotPasswordDto {
+  @IsEmail()
+  @MaxLength(USER_EMAIL_MAX_LENGTH)
+  email!: string;
+}
