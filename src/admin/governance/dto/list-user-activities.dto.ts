92871f2ea5a89c9dbe0878adc38a8f7070c49e51
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 22:26:10 +0200

    Validate user activity queries

---
+++ src/admin/governance/dto/list-user-activities.dto.ts
@@ -0,0 +1,39 @@
+import {
+  Type,
+} from 'class-transformer';
+import {
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
+import {
+  GOVERNANCE_DEFAULT_LIMIT,
+  GOVERNANCE_DEFAULT_PAGE,
+  GOVERNANCE_MAX_LIMIT,
+} from '../admin-governance.constants';
+
+export class ListUserActivitiesDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page: number =
+    GOVERNANCE_DEFAULT_PAGE;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(GOVERNANCE_MAX_LIMIT)
+  limit: number =
+    GOVERNANCE_DEFAULT_LIMIT;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  action?: string;
+}
