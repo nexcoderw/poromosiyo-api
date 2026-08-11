74f908ff17fa9d18beabc5c8d6f0eabfca84ae22
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 22:26:10 +0200

    Validate account blocking requests

---
+++ src/admin/governance/dto/block-account.dto.ts
@@ -0,0 +1,12 @@
+import {
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class BlockAccountDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(500)
+  reason!: string;
+}
