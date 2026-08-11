dab95079bc00415dbe9969ddde5bce301b68c797
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 21:59:21 +0200

    Wire catalog publication and activity tracking

--- src/admin/catalog/admin-catalog.module.ts
+++ src/admin/catalog/admin-catalog.module.ts
@@ -1,43 +1,18 @@
-import {
-  Module,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import {
-  AuthModule,
-} from '../../auth/auth.module';
-import {
-  DatabaseModule,
-} from '../../database/database.module';
-import {
-  AdminBrandsController,
-} from './controllers/admin-brands.controller';
-import {
-  AdminCategoriesController,
-} from './controllers/admin-categories.controller';
-import {
-  AdminProductImagesController,
-} from './controllers/admin-product-images.controller';
-import {
-  AdminProductsController,
-} from './controllers/admin-products.controller';
-import {
-  AdminBrandsService,
-} from './services/admin-brands.service';
-import {
-  AdminCategoriesService,
-} from './services/admin-categories.service';
-import {
-  AdminProductImagesService,
-} from './services/admin-product-images.service';
-import {
-  AdminProductsService,
-} from './services/admin-products.service';
+import { AuthModule } from '../../auth/auth.module';
+import { DatabaseModule } from '../../database/database.module';
+import { AdminBrandsController } from './controllers/admin-brands.controller';
+import { AdminCategoriesController } from './controllers/admin-categories.controller';
+import { AdminProductImagesController } from './controllers/admin-product-images.controller';
+import { AdminProductsController } from './controllers/admin-products.controller';
+import { AdminBrandsService } from './services/admin-brands.service';
+import { AdminCategoriesService } from './services/admin-categories.service';
+import { AdminProductImagesService } from './services/admin-product-images.service';
+import { AdminProductsService } from './services/admin-products.service';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    AuthModule,
-  ],
+  imports: [DatabaseModule, AuthModule],
 
   controllers: [
     AdminCategoriesController,
