e9fddaf636cd64485873c64cb83deb7fdad3f412
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 22:26:21 +0200

    Establish admin governance module

---
+++ src/admin/governance/admin-governance.module.ts
@@ -0,0 +1,48 @@
+import {
+  Module,
+} from '@nestjs/common';
+
+import {
+  AuthModule,
+} from '../../auth/auth.module';
+import {
+  DatabaseModule,
+} from '../../database/database.module';
+import {
+  AdminAdminsController,
+} from './controllers/admin-admins.controller';
+import {
+  AdminCustomersController,
+} from './controllers/admin-customers.controller';
+import {
+  AccountGovernanceService,
+} from './services/account-governance.service';
+import {
+  AdminAdminsService,
+} from './services/admin-admins.service';
+import {
+  AdminCustomersService,
+} from './services/admin-customers.service';
+import {
+  AdminUserActivitiesService,
+} from './services/admin-user-activities.service';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    AuthModule,
+  ],
+
+  controllers: [
+    AdminCustomersController,
+    AdminAdminsController,
+  ],
+
+  providers: [
+    AccountGovernanceService,
+    AdminCustomersService,
+    AdminAdminsService,
+    AdminUserActivitiesService,
+  ],
+})
+export class AdminGovernanceModule {}
