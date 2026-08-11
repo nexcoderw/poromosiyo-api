2696a4cd9ab7dda59bd887fbd9cc3a85be68b937
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 20:23:11 +0200

    Validate password reset requests

---
+++ src/auth/dto/reset-password.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+import {
+  AUTH_PASSWORD_MAX_LENGTH,
+  AUTH_PASSWORD_MIN_LENGTH,
+} from '../auth.constants';
+
+export class ResetPasswordDto {
+  @IsString()
+  @MinLength(32)
+  @MaxLength(512)
+  token!: string;
+
+  @IsString()
+  @MinLength(AUTH_PASSWORD_MIN_LENGTH)
+  @MaxLength(AUTH_PASSWORD_MAX_LENGTH)
+  newPassword!: string;
+
+  @IsString()
+  @MinLength(AUTH_PASSWORD_MIN_LENGTH)
+  @MaxLength(AUTH_PASSWORD_MAX_LENGTH)
+  confirmPassword!: string;
+}
