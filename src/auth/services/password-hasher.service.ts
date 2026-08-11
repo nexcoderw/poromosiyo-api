f5ffa4fcda3de439ed8bc522d86498793da6d6ee
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 19:44:31 +0200

    Add Argon2id password hashing service

---
+++ src/auth/services/password-hasher.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+
+@Injectable()
+export class PasswordHasherService {
+  async hash(
+    password: string,
+  ): Promise<string> {
+    return argon2.hash(password, {
+      type: argon2.argon2id,
+      memoryCost: 19456,
+      timeCost: 2,
+      parallelism: 1,
+    });
+  }
+
+  async verify(
+    passwordHash: string,
+    password: string,
+  ): Promise<boolean> {
+    return argon2.verify(
+      passwordHash,
+      password,
+    );
+  }
+}
