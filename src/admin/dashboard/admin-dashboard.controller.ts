9f284421db9d34570aee46ed40c64a1e9515ea6d
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-12 16:17:26 +0200

    Format admin dashboard routes

--- src/admin/dashboard/admin-dashboard.controller.ts
+++ src/admin/dashboard/admin-dashboard.controller.ts
@@ -1,40 +1,21 @@
-import {
-  Controller,
-  Get,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
-import {
-  RequireAuthRoles,
-} from '../../auth/decorators/require-auth-roles.decorator';
-import {
-  AuthRoleGuard,
-} from '../../auth/guards/auth-role.guard';
-import {
-  JwtAuthGuard,
-} from '../../auth/guards/jwt-auth.guard';
-import {
-  AdminDashboardService,
-} from './admin-dashboard.service';
+import { RequireAuthRoles } from '../../auth/decorators/require-auth-roles.decorator';
+import { AuthRoleGuard } from '../../auth/guards/auth-role.guard';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { AdminDashboardService } from './admin-dashboard.service';
 
 @Controller({
   path: 'admin/dashboard',
   version: '1',
 })
 @RequireAuthRoles('ADMIN')
-@UseGuards(
-  JwtAuthGuard,
-  AuthRoleGuard,
-)
+@UseGuards(JwtAuthGuard, AuthRoleGuard)
 export class AdminDashboardController {
-  constructor(
-    private readonly dashboard:
-      AdminDashboardService,
-  ) {}
+  constructor(private readonly dashboard: AdminDashboardService) {}
 
   @Get()
   getDashboard() {
-    return this.dashboard
-      .getDashboard();
+    return this.dashboard.getDashboard();
   }
 }
