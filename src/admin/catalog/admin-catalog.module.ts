3a95e4f754ed6715c00096bd20365995bfb7237c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-12 16:15:09 +0200

    Wire Store catalog administration

--- src/admin/catalog/admin-catalog.module.ts
+++ src/admin/catalog/admin-catalog.module.ts
@@ -1,53 +1,28 @@
-import {
-  Module,
-} from '@nestjs/common';
+import { AdminStoresService } from './services/admin-stores.service';
+import { AdminProductStoreService } from './services/admin-product-store.service';
+import { AdminStoresController } from './controllers/admin-stores.controller';
+import { Module } from '@nestjs/common';
 
-import {
-  AuthModule,
-} from '../../auth/auth.module';
-import {
-  DatabaseModule,
-} from '../../database/database.module';
-import {
-  AdminBrandsController,
-} from './controllers/admin-brands.controller';
-import {
-  AdminCategoriesController,
-} from './controllers/admin-categories.controller';
-import {
-  AdminProductImagesController,
-} from './controllers/admin-product-images.controller';
-import {
-  AdminProductsController,
-} from './controllers/admin-products.controller';
-import {
-  AdminBrandsService,
-} from './services/admin-brands.service';
-import {
-  AdminCategoriesService,
-} from './services/admin-categories.service';
-import {
-  AdminProductArchiveService,
-} from './services/admin-product-archive.service';
-import {
-  AdminProductImagesService,
-} from './services/admin-product-images.service';
-import {
-  AdminProductPublicationService,
-} from './services/admin-product-publication.service';
-import {
-  AdminProductsService,
-} from './services/admin-products.service';
+import { AuthModule } from '../../auth/auth.module';
+import { DatabaseModule } from '../../database/database.module';
+import { AdminBrandsController } from './controllers/admin-brands.controller';
+import { AdminCategoriesController } from './controllers/admin-categories.controller';
+import { AdminProductImagesController } from './controllers/admin-product-images.controller';
+import { AdminProductsController } from './controllers/admin-products.controller';
+import { AdminBrandsService } from './services/admin-brands.service';
+import { AdminCategoriesService } from './services/admin-categories.service';
+import { AdminProductArchiveService } from './services/admin-product-archive.service';
+import { AdminProductImagesService } from './services/admin-product-images.service';
+import { AdminProductPublicationService } from './services/admin-product-publication.service';
+import { AdminProductsService } from './services/admin-products.service';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    AuthModule,
-  ],
+  imports: [DatabaseModule, AuthModule],
 
   controllers: [
     AdminCategoriesController,
     AdminBrandsController,
+    AdminStoresController,
     AdminProductsController,
     AdminProductImagesController,
   ],
@@ -55,9 +30,11 @@ import {
   providers: [
     AdminCategoriesService,
     AdminBrandsService,
+    AdminStoresService,
     AdminProductsService,
     AdminProductImagesService,
     AdminProductPublicationService,
+    AdminProductStoreService,
     AdminProductArchiveService,
   ],
 })
