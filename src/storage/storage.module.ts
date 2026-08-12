ff90738263f349e2bd1cc6c322c91245b9e42b30
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-12 16:49:44 +0200

    Expose image storage through a global module

---
+++ src/storage/storage.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from '@nestjs/common';
+
+import { ImageStorageService } from './image-storage.service';
+
+@Global()
+@Module({
+  providers: [ImageStorageService],
+  exports: [ImageStorageService],
+})
+export class StorageModule {}
