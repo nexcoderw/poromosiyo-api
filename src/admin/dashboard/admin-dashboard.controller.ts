47191c0e0842ffce1d93f0317c97a0a094ab1222
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-12 15:42:27 +0200

    Expose admin dashboard endpoint

---
+++ src/admin/dashboard/admin-dashboard.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+
+import {
+  RequireAuthRoles,
+} from '../../auth/decorators/require-auth-roles.decorator';
+import {
+  AuthRoleGuard,
+} from '../../auth/guards/auth-role.guard';
+import {
+  JwtAuthGuard,
+} from '../../auth/guards/jwt-auth.guard';
+import {
+  AdminDashboardService,
+} from './admin-dashboard.service';
+
+@Controller({
+  path: 'admin/dashboard',
+  version: '1',
+})
+@RequireAuthRoles('ADMIN')
+@UseGuards(
+  JwtAuthGuard,
+  AuthRoleGuard,
+)
+export class AdminDashboardController {
+  constructor(
+    private readonly dashboard:
+      AdminDashboardService,
+  ) {}
+
+  @Get()
+  getDashboard() {
+    return this.dashboard
+      .getDashboard();
+  }
+}
