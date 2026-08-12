b270cbd0b40c8bb63e43ce5fc73f7a85e49d24d3
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-12 16:49:38 +0200

    Define image upload and optimization limits

---
+++ src/storage/image-storage.constants.ts
@@ -0,0 +1,14 @@
+export const IMAGE_UPLOAD_FIELD = 'image';
+export const IMAGE_UPLOAD_MAX_BYTES = 12 * 1024 * 1024;
+export const IMAGE_OUTPUT_MAX_BYTES = 500 * 1024;
+export const IMAGE_MAX_DIMENSION = 1600;
+export const IMAGE_WEBP_QUALITY = 82;
+export const PRODUCT_IMAGE_LIMIT = 10;
+
+export const ACCEPTED_IMAGE_MIME_TYPES = new Set([
+  'image/jpeg',
+  'image/png',
+  'image/webp',
+  'image/heic',
+  'image/heif',
+]);
